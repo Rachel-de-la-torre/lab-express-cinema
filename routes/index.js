03cecb0fca03001e7928a9cd95804450d4488e91
Rachel de la Torre <Rachel.delatorre1@gmail.com>
2020-06-30 09:21:10 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res) => {
+  Movie.find()
+    .then((moviesFromDatabase) => {
+      console.log(moviesFromDatabase);
+      res.render("movies", { movieList: moviesFromDatabase });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/movies/:id", (req, res) => {
+  console.log(req.params.id);
+  Movie.findById(req.params.id)
+    .then((movie) => {
+      console.log(movie);
+      res.render("movieDetails", { movie });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 module.exports = router;
